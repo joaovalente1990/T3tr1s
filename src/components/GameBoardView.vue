dfd981f8378c8a1ad9b9b359ff6b461e3c37af0d
Joao Valente <jhrvalente1990@gmail.com>
2023-06-25 18:44:58 +0100

    Added the assets folder and created the file with the routes.

---
+++ src/components/GameBoardView.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container>
+        <p>Gameboard</p>
+    </v-container>
+    
+</template>
+
+<script>
+export default {
+
+    data(){
+
+        return {
+
+        }
+
+    },
+    computed: {
+
+    },
+    watch: {
+        
+    },
+    created(){
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
